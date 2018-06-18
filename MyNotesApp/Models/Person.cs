a67a575b0388439a791226c3f4341f23731dacbf
Lisa Hare <hare.lisa@gmail.com>
2018-06-18 12:17:34 -0700

    added Person, StickyNote, and Note classes

---
+++ MyNotesApp/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace MyNotesApp.Models
+{
+    class Person
+    {
+        /// <summary>
+        /// Person's name
+        /// </summary>
+        public string FriendlyName { get; set; }
+        /// <summary>
+        /// Set true if Person already has a profile image setup
+        /// </summary>
+        public bool IsProfileImage { get; set; }
+        /// <summary>
+        /// Store Person's image of choice
+        /// </summary>
+        public BitmapImage Image { get; set; }
+
+    }
+}
---
+++ MyNotesApp/Models/StickyNote.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNotesApp.Models
+{
+    class StickyNote
+    {
+        /// <summary>
+        /// Person the note is for
+        /// </summary>
+        public Person NoteIsFor { get; set; }
+        /// <summary>
+        /// The text on the note
+        /// </summary>
+        public string NoteText { get; set; }
+
+
+    }
+}
